4d97ef1e72710e2e814a67a8972590ca589e7087
Wurly <48784425+wurly200a@users.noreply.github.com>
2024-12-01 09:27:34 +0900

    Build with w5500 configuration

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # w5500-on-nuttx-esp32
 W5500 ethernet module on NuttX and ESP32
+
+## Write FlashROM
+
+### Setup
+
+```
+python3 -m venv ./venv
+. venv/bin/activate
+pip3 install esptool==v4.7.0
+deactivate
+```
+
+### Write
+
+```
+. venv/bin/activate
+PORT=/dev/ttyUSB0
+esptool.py --chip esp32 --port ${PORT} --baud 921600 --before default_reset erase_flash;
+esptool.py --chip esp32 --port ${PORT} --baud 921600 write_flash 0x1000 nuttx/bootloader-esp32.bin 0x8000 nuttx/partition-table-esp32.bin 0x10000 nuttx/nuttx.bin
+deactivate
+```
--- build.sh
+++ build.sh
@@ -43,24 +43,24 @@ function configure() {
         git clone ${NUTTX_APPS_GIT_URL} -b ${NUTTX_APPS_GIT_TAG} ${NUTTX_APPS_DIR}
     fi
 
-#    cp replacement_files/boards.xtensa.esp32.common.src.esp32_ssd1306.c ${NUTTX_DIR}/boards/xtensa/esp32/common/src/esp32_ssd1306.c
+    cp replacement_files/boards.xtensa.esp32.esp32-devkitc.src.esp32_w5500.c ${NUTTX_DIR}/boards/xtensa/esp32/esp32-devkitc/src/esp32_w5500.c
 
     cd nuttx
     ./tools/configure.sh -l ${BOARD}:${CONFIG}
 
-#    #
-#    # Debug Options
-#    #
-#    kconfig-tweak --enable DEBUG_FEATURES
-#
-#    #
-#    # Debug SYSLOG Output Controls
-#    #
-#    kconfig-tweak --enable DEBUG_ERROR
-#    kconfig-tweak --enable DEBUG_WARN
-#    kconfig-tweak --enable DEBUG_INFO
-#    kconfig-tweak --enable DEBUG_ASSERTIONS
-#
+    #
+    # Debug Options
+    #
+    kconfig-tweak --enable DEBUG_FEATURES
+
+    #
+    # Debug SYSLOG Output Controls
+    #
+    kconfig-tweak --enable DEBUG_ERROR
+    kconfig-tweak --enable DEBUG_WARN
+    kconfig-tweak --enable DEBUG_INFO
+    kconfig-tweak --enable DEBUG_ASSERTIONS
+
 #    #
 #    # Subsystem Debug Options
 #    #
@@ -82,17 +82,39 @@ function configure() {
 #    kconfig-tweak --enable DEBUG_I2C_WARN
 #    kconfig-tweak --enable DEBUG_I2C_INFO
 #
-#    kconfig-tweak --enable DEBUG_SYMBOLS
+
+    kconfig-tweak --enable DEBUG_SPI
+    kconfig-tweak --enable DEBUG_SPI_ERROR
+    kconfig-tweak --enable DEBUG_SPI_WARN
+    kconfig-tweak --enable DEBUG_SPI_INFO
+
+    kconfig-tweak --enable DEBUG_GPIO
+    kconfig-tweak --enable DEBUG_GPIO_ERROR
+    kconfig-tweak --enable DEBUG_GPIO_WARN
+    kconfig-tweak --enable DEBUG_GPIO_INFO
+
+    kconfig-tweak --enable DEBUG_NET
+    kconfig-tweak --enable DEBUG_NET_ERROR
+    kconfig-tweak --enable DEBUG_NET_WARN
+    kconfig-tweak --enable DEBUG_NET_INFO
+
+    kconfig-tweak --enable DEBUG_IRQ
+    kconfig-tweak --enable DEBUG_IRQ_ERROR
+    kconfig-tweak --enable DEBUG_IRQ_WARN
+    kconfig-tweak --enable DEBUG_IRQ_INFO
+
+    kconfig-tweak --enable DEBUG_ASSERTIONS
+    kconfig-tweak --enable DEBUG_SYMBOLS
 #
 #    #
 #    # System Logging
 #    #
 #
-#    #
-#    # SYSLOG channels
-#    #
-#    kconfig-tweak --set-str SYSLOG_DEVPATH "/dev/console"
-#    kconfig-tweak --enable SYSLOG_CONSOLE
+    #
+    # SYSLOG channels
+    #
+    kconfig-tweak --set-str SYSLOG_DEVPATH "/dev/console"
+    kconfig-tweak --enable SYSLOG_CONSOLE
 
 #    # hello
 #    kconfig-tweak --enable APP_HELLO
@@ -104,9 +126,10 @@ function configure() {
     kconfig-tweak --enable ARCH_CHIP_ESP32WROVER
 
 #      ESP32 Peripheral Selection  --->
-#        [*] SPI 3
+#        [*] SPI 2
+#        [ ] SPI 3
 #        [*] SPI RAM
-#    kconfig-tweak --enable ESP32_SPI3
+    kconfig-tweak --enable ESP32_SPI2
     kconfig-tweak --enable ESP32_SPIRAM
 
 #        [ ] I2C 0
@@ -126,17 +149,23 @@ function configure() {
 #        (25) I2C1 SDA Pin
 
 #      SPI Configuration  --->
-#        (14) SPI3 CS Pin
-#        (18) SPI3 CLK Pin
-#        (23) SPI3 MOSI Pin
-#        (19) SPI3 MISO Pin
+#        [ ] SPI software CS
+#        [ ]   User defined CS
+#        [ ] SPI2 use DMA
+#        (2) SPI master DMA description number
+#        (15) SPI2 CS Pin
+#        (14) SPI2 CLK Pin
+#        (13) SPI2 MOSI Pin
+#        (12) SPI2 MISO Pin
 #        SPI3 master I/O mode (Read & Write)  --->
 #          (X) Read & Write
-#    kconfig-tweak --set-val ESP32_SPI3_CSPIN 14
-#    kconfig-tweak --set-val ESP32_SPI3_CLKPIN 18
-#    kconfig-tweak --set-val ESP32_SPI3_MOSIPIN 23
-#    kconfig-tweak --set-val ESP32_SPI3_MISOPIN 19
-#    kconfig-tweak --enable ESP32_SPI3_MASTER_IO_RW
+    kconfig-tweak --enable ESP32_SPI_SWCS
+    kconfig-tweak --disable ESP32_SPI2_DMA
+    kconfig-tweak --set-val ESP32_SPI2_CSPIN 15
+    kconfig-tweak --set-val ESP32_SPI2_CLKPIN 14
+    kconfig-tweak --set-val ESP32_SPI2_MOSIPIN 13
+    kconfig-tweak --set-val ESP32_SPI2_MISOPIN 12
+    kconfig-tweak --enable ESP32_SPI2_MASTER_IO_RW
 #      SPI Flash Configuration  --->
 #        (0x300000) Storage MTD base adddress in SPI Flash
 #        (0x100000) Storage MTD size in SPI Flash
@@ -168,26 +197,33 @@ function configure() {
     kconfig-tweak --set-val MQ_MAXMSGSIZE 64
 
 #    Device Drivers  --->
-#      [*] I2C Driver Support  --->
+#      [ ] I2C Driver Support  --->
 
 #      -*- SPI Driver Support  --->
 #        [*] SPI exchange
 #        [*] SPI CMD/DATA
+    kconfig-tweak --enable SPI
+    kconfig-tweak --enable SPI_EXCHANGE
+    kconfig-tweak --enable SPI_CMDDATA
+#
+#      [ ] Video Device Support  --->
+#        [ ] Framebuffer character driver
 #
 #
-#      [*] Video Device Support  --->
-#        [*] Framebuffer character driver
-#
-#
-#      [*] LCD Driver Support  --->
-#        [*] Graphic LCD Driver Support  --->
-#          [*] LCD framebuffer front end
-#          [*] LCD driver selection  --->
-#            [*] Generic SPI Interface Driver (for ILI9341 or others)
+#      [ ] LCD Driver Support  --->
+#        [ ] Graphic LCD Driver Support  --->
+#          [ ] LCD framebuffer front end
+#          [ ] LCD driver selection  --->
+#            [ ] Generic SPI Interface Driver (for ILI9341 or others)
 #            (1) Number of SSD1306 displays 
 #                SSD1306 Interface (SSD1306 on I2C Interface)  --->
 
-    # LCD driver selection
+#      [ ] Network Device/PHY Support  --->
+
+#       [*]   WIZnet W5500 Support  --->
+#         (1)   Number of physical W5500 devices
+    kconfig-tweak --enable NET_W5500
+    kconfig-tweak --set-val W5500_NINTERFACES 1
 
 #    Timer Driver Support
 #
@@ -195,7 +231,12 @@ function configure() {
 #
 #    Networking Support  --->
 #
-#
+    kconfig-tweak --disable NETDEV_LATEINIT
+
+    kconfig-tweak --disable ESP32_WIFI
+    kconfig-tweak --disable DRIVERS_WIRELESS
+    kconfig-tweak --disable DRIVERS_IEEE80211
+
 #    File Systems  --->
 #      [*] ROMFS file system
 #    kconfig-tweak --enable FS_ROMFS
@@ -246,7 +287,18 @@ function configure() {
 #
 
 #      System Libraries and NSH Add-Ons  --->
-#        [*] I2C tool  --->
+#        [*] SPI tool  --->
+    kconfig-tweak --enable SYSTEM_SPITOOL
+    kconfig-tweak --set-str SPITOOL_PROGNAME "spi"
+    kconfig-tweak --set-val SPITOOL_PRIORITY 100
+    kconfig-tweak --set-val SPITOOL_STACKSIZE 4096
+    kconfig-tweak --set-val SPITOOL_MINBUS 0
+    kconfig-tweak --set-val SPITOOL_MAXBUS 3
+    kconfig-tweak --set-val SPITOOL_DEFFREQ 4000000
+    kconfig-tweak --set-val SPITOOL_DEFCMD 0
+    kconfig-tweak --set-val SPITOOL_DEFMODE 0
+    kconfig-tweak --set-val SPITOOL_DEFWIDTH 8
+    kconfig-tweak --set-val SPITOOL_DEFWORDS 1
 
 #    kconfig-tweak --enable PSEUDOFS_SOFTLINKS
 #    kconfig-tweak --enable FS_RAMMAP
@@ -297,7 +349,7 @@ function build_bootloader() {
 
 function build_partition_table() {
     cd ${NUTTX_DIR}
-    python3 ../partition/gen_esp32part.py ../partition/esp32-partitions.csv partition-table-esp32.bin
+    gen_esp32part.py ../partition/esp32-partitions.csv partition-table-esp32.bin
     cd ..
 }
 
@@ -316,7 +368,7 @@ if [ -n "$ESP_IDF_VERSION" ]; then
 else
     echo "This script is expected to run using docker container which included ESP_IDF"
     echo "Please run:"
-    echo "docker run --rm -it -v \${PWD}:/mnt/work -w /mnt/work ghcr.io/wurly200a/builder-esp32/esp-idf-v5.3:latest"
+    echo "docker run --rm -it --user 1000:1000 -v \${PWD}:/mnt/work -w /mnt/work ghcr.io/wurly200a/builder-esp32/esp-idf-v5.3:latest"
     echo "then"
     echo ". /opt/esp-idf/export.sh"
     exit
